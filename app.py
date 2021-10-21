fe55120d346db8bef1786ca3bd34232b6b8d5ccd
xsong0306 <86331397+xsong0306@users.noreply.github.com>
2021-10-21 08:27:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 import pickle
 import streamlit as st
-# import spotipy
-# from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 import func
-# auth_manager = SpotifyClientCredentials()
-# sp = spotipy.Spotify(auth_manager=auth_manager)
-df = pd.read_csv('songs.csv')
 
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+df = pd.read_csv('songs.csv')
 
 st.title('Music Recommend System')
 musics_dict = pickle.load(open('music_dict.pkl', 'rb'))
 musics = pd.DataFrame(musics_dict)
 
 selected_artist = st.selectbox(
-    "Type or select a music from the dropdown",
+    "Type or select an artist from the dropdown",
     musics['artist_name'].values,
     index=8000
 )
 
 selected_music = st.selectbox(
-    "Type or select a music from the dropdown",
+    "Type or select a song from the dropdown",
     musics[musics['artist_name']==selected_artist]['track_name'].values,
     index=8000
 )
